1e384300d46a30b0ab4eec9eb4cc195d2a6330d2
unknown <wwhurin@e-mirim.hs.kr>
2018-06-25 09:01:47 +0900

    LOgout

--- app/src/main/java/com/ghen61/agabankh/SettingActivity.java
+++ app/src/main/java/com/ghen61/agabankh/SettingActivity.java
@@ -1,6 +1,8 @@
 package com.ghen61.agabankh;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -41,14 +43,23 @@ public class SettingActivity extends AppCompatActivity{
 
 
                 switch(i){
-
                     case 0: intent = new Intent(SettingActivity.this,CreateActivity.class);
                             startActivity(intent);
                             finish();break;
                     case 1:
-                        Toast.makeText(SettingActivity.this, "준비중입니다.", Toast.LENGTH_SHORT).show(); break;
+                        Toast.makeText(SettingActivity.this, "준비중입니다.", Toast.LENGTH_SHORT).show();
+                        break;
                     case 2:
-                        Toast.makeText(SettingActivity.this, "준비중입니다.", Toast.LENGTH_SHORT).show(); break;
+                        //SharedPreferences를 불러옵니다. 메인에서 만든 이름으로
+                        Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        SharedPreferences auto = getSharedPreferences("appData", Activity.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = auto.edit();
+                        //editor.clear()는 auto에 들어있는 모든 정보를 기기에서 지웁니다.
+                        editor.clear();
+                        editor.commit();
+                        Toast.makeText(SettingActivity.this, "로그아웃.", Toast.LENGTH_SHORT).show();
+                        finish();break;
                     case 3:
                         Toast.makeText(SettingActivity.this, "준비중입니다.", Toast.LENGTH_SHORT).show(); break;
 
